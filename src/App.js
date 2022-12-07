0bf8c688f8a03ca9262a08cf4da5610c85dc7b0b
manasa <athallurmanasa33@gmail.com>
2022-12-06 23:27:23 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br></br>
         <span>
           <span>Learn </span>
           <a
